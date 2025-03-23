adc4f24bd5a2dc37aa4ea4d2e25c4cb4ccffeff7
levink <levinkostya@yandex.ru>
2025-03-23 10:30:21 +0100

    Little refactoring

--- src/main.cpp
+++ src/main.cpp
@@ -3,67 +3,130 @@
 #include "ffmpeg.h"
 #include "lodepng.h"
 
+namespace png {
+    static void saveGrayPng(const char* fileName, const AVFrame* frame) {
+        const auto width = frame->width;
+        const auto height = frame->height;
+        const auto lineSize = frame->linesize[0];
+
+        std::vector<uint8_t> pixelsGray(width * height * 4, 0);
+        uint8_t* lineBegin = frame->data[0];
+        for (int y = 0; y < frame->height; y++) {
+            for (int x = 0; x < frame->width; x++) {
+                uint8_t gray = lineBegin[x];
+
+                auto base = (y * width + x) * 4;
+                auto rIndex = base + 0;
+                auto gIndex = base + 1;
+                auto bIndex = base + 2;
+                auto aIndex = base + 3;
+
+                pixelsGray[rIndex] = gray;
+                pixelsGray[gIndex] = gray;
+                pixelsGray[bIndex] = gray;
+                pixelsGray[aIndex] = 255;
+            }
+            lineBegin += lineSize;
+        }
+
+        unsigned retPNG = lodepng::encode(fileName, pixelsGray, width, height, LodePNGColorType::LCT_RGBA);
+        if (retPNG) {
+            std::cout << "lodepng::encode() on gray: Error code=" << retPNG << std::endl;
+        }
+    }
+}
+
 struct FileInfo {
     AVFormatContext* formatContext = nullptr;
     AVCodecContext* codecContext = nullptr;
-    int video_stream_index = -1;
-
+    SwsContext* swsContext = nullptr;
+    int videoIndex = -1;
+    const AVCodec* codec = nullptr;
+    AVPacket* packet = nullptr;
+    AVFrame* frame = nullptr;
+    
     std::string error;
     bool openFile(const char* fileName);
     void processFrame(AVFrame* frame);
+
+    ~FileInfo() {
+        if (formatContext) {
+            avformat_free_context(formatContext);
+        }
+        if (codecContext) {
+            avcodec_free_context(&codecContext);
+        }
+        if (swsContext) {
+            sws_freeContext(swsContext);
+        }
+        if (packet) {
+            av_packet_free(&packet);
+        }
+        if (frame) {
+            av_frame_free(&frame);
+        }        
+    }
 };
 
 bool FileInfo::openFile(const char* fileName) {
 
     formatContext = avformat_alloc_context();
     if (!formatContext) {
-        error = "FileInfo::openFile: cannot allocate formatContext";
+        error = "formatContext bad alloc";
         return false;
     }
 
     int ret = avformat_open_input(&formatContext, fileName, nullptr, nullptr);
     if (ret < 0) {
-        error = "openFile(): cannot open input file";
+        error = "Cannot open input file";
         return false;
     }
 
     ret = avformat_find_stream_info(formatContext, nullptr);
     if (ret < 0) {
-        error = "openFile(): stream info not found";
+        error = "Stream info not found";
         return false;
     }
 
-    const AVCodec* codec = nullptr;
     ret = av_find_best_stream(formatContext, AVMEDIA_TYPE_VIDEO, -1, -1, &codec, 0);
     if (ret < 0) {
-        error = "openInputFile(): video stream not found";
+        error = "Video stream not found";
         return false;
     } 
-    video_stream_index = ret;
+    videoIndex = ret;
 
     codecContext = avcodec_alloc_context3(codec);
     if (!codecContext) {
-        error = "openInputFile(): bad alloc for codec context";
+        error = "codecContext bad alloc";
         return false;
     }
 
-    const auto& stream = formatContext->streams[video_stream_index];
+    const auto& stream = formatContext->streams[videoIndex];
     ret = avcodec_parameters_to_context(codecContext, stream->codecpar);
     if (ret < 0) {
-        error = "openInputFile(): bad copy params to context";
+        error = "Bad copy params from stream to codec context";
+        return false;
+    }
+
+    swsContext = sws_getContext(
+        codecContext->width, codecContext->height, codecContext->pix_fmt,
+        codecContext->width, codecContext->height, AV_PIX_FMT_RGB0,
+        SWS_BILINEAR, nullptr, nullptr, nullptr);
+    if (!swsContext) {
+        error = "swsContext bad alloc";
         return false;
     }
 
     ret = avcodec_open2(codecContext, codec, nullptr);
     if (ret < 0) {
-        error = "openInputFile(): cannot open video decoder";
+        error = "Cannot open video decoder";
         return false;
     }
 
-    AVPacket* packet = av_packet_alloc();
-    AVFrame* frame = av_frame_alloc();
+    packet = av_packet_alloc();
+    frame = av_frame_alloc();
     if (!packet || !frame) {
-        error = "openInputFile(): bad alloc packet or frame";
+        error = "Packet or frame bad alloc";
         return false;
     }
 
@@ -71,7 +134,7 @@ bool FileInfo::openFile(const char* fileName) {
     int frameReads = 101;
     while (av_read_frame(formatContext, packet) >= 0 && frameReads > 0) {
         int response = avcodec_send_packet(codecContext, packet);
-        if (packet->stream_index != video_stream_index) {
+        if (packet->stream_index != videoIndex) {
             av_packet_unref(packet);
             continue;
         }
@@ -93,7 +156,6 @@ bool FileInfo::openFile(const char* fileName) {
             else {
                 processFrame(frame);
             }
-            
         }
         av_packet_unref(packet);
     }
@@ -102,44 +164,9 @@ bool FileInfo::openFile(const char* fileName) {
 }
 void FileInfo::processFrame(AVFrame* frame) {
 
-    auto width = frame->width;
-    auto height = frame->height;
-    auto lineSize = frame->linesize[0];
-
-    std::vector<uint8_t> pixelsGray(width * height * 4, 0);
-    uint8_t* lineBegin = frame->data[0];
-    for (int y = 0; y < frame->height; y++) {
-        for (int x = 0; x < frame->width; x++) {
-            uint8_t gray = lineBegin[x];
-
-            auto base = (y * width + x) * 4;
-            auto rIndex = base + 0;
-            auto gIndex = base + 1;
-            auto bIndex = base + 2;
-            auto aIndex = base + 3;
-
-            pixelsGray[rIndex] = gray;
-            pixelsGray[gIndex] = gray;
-            pixelsGray[bIndex] = gray;
-            pixelsGray[aIndex] = 255;
-        }
-
-        lineBegin += lineSize;
-    }
-
-    unsigned retPNG = lodepng::encode("./testGray.png", pixelsGray, width, height, LodePNGColorType::LCT_RGBA);
-    if (retPNG) {
-        std::cout << "lodepng::encode() on gray: Error code=" << retPNG << std::endl;
-    }
-
-    SwsContext* swsContext = sws_getContext(
-        width, height, codecContext->pix_fmt,
-        width, height, AV_PIX_FMT_RGB0,
-        SWS_BILINEAR, nullptr, nullptr, nullptr);
-    if (!swsContext) {
-        std::cout << "sws_getContext() return nullptr" << std::endl;
-        return;
-    }
+    const auto width = frame->width;
+    const auto height = frame->height;
+    const auto lineSize = frame->linesize[0];
 
     int destLineSize[4] = { width * 4, 0, 0, 0 };           // *3 maybe?
     uint8_t* pixelsRGBA = new uint8_t[width * height * 4];  // *3 maybe?
@@ -149,10 +176,7 @@ void FileInfo::processFrame(AVFrame* frame) {
         std::cout << "sws_scale() return " << ret << std::endl;
     }
 
-    sws_freeContext(swsContext);
-    swsContext = nullptr;
-
-    retPNG = lodepng::encode("./testRGB.png", pixelsRGBA, width, height, LodePNGColorType::LCT_RGBA);
+    auto retPNG = lodepng::encode("./testRGB.png", pixelsRGBA, width, height, LodePNGColorType::LCT_RGBA);
     if (retPNG) {
         std::cout << "lodepng::encode() on rgb: Error code=" << retPNG << std::endl;
     }
@@ -195,11 +219,9 @@ int main() {
 	}*/
 
     av_log_set_level(AV_LOG_FATAL);
-
-	const char* fileName = "C:/Users/Konst/Desktop/k/IMG_3504.MOV";
    
     FileInfo info;
-    bool opened = info.openFile(fileName);
+    bool opened = info.openFile("C:/Users/Konst/Desktop/k/IMG_3504.MOV");
     if (!opened) {
         std::cout << info.error << std::endl;
         return -1;
@@ -207,34 +229,3 @@ int main() {
 
 	return 0;
 }
-
-//static void display_frame(const AVFrame* frame, AVRational time_base)
-//{
-//    int x, y;
-//    uint8_t* p0, * p;
-//    int64_t delay;
-//
-//    if (frame->pts != AV_NOPTS_VALUE) {
-//        if (last_pts != AV_NOPTS_VALUE) {
-//            /* sleep roughly the right amount of time;
-//             * usleep is in microseconds, just like AV_TIME_BASE. */
-//            delay = av_rescale_q(frame->pts - last_pts,
-//                time_base, AV_TIME_BASE_Q);
-//            if (delay > 0 && delay < 1000000)
-//                usleep(delay);
-//        }
-//        last_pts = frame->pts;
-//    }
-//
-//    /* Trivial ASCII grayscale display. */
-//    p0 = frame->data[0];
-//    puts("\033c");
-//    for (y = 0; y < frame->height; y++) {
-//        p = p0;
-//        for (x = 0; x < frame->width; x++)
-//            putchar(" .-+#"[*(p++) / 52]);
-//        putchar('\n');
-//        p0 += frame->linesize[0];
-//    }
-//    fflush(stdout);
-//}
